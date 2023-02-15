19a5ba3ff9bbcf6ae4e457168e2256c7a2b763c0
MazzomesClone <49418843+MazzomesClone@users.noreply.github.com>
2023-02-16 01:58:32 +1300

    Improved UI
    Improved UI, larger result display, fade between results

--- diceroll.js
+++ diceroll.js
@@ -3,3 +3,11 @@ function diceRoll(sides) {
     max = Math.floor(sides);
     return Math.floor(Math.random() * (sides) + 1);
 }
+
+var results = []
+
+for (let i = 0; i < 200; i++) {
+    results.push(diceRoll(20))
+}
+
+console.log(results)
